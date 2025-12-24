923c08b021bfa93723468d79e6562ef8b8676639
vovik541 <conanzhill@gmail.com>
2025-12-23 19:34:53 -0800

    task done

---
+++ tasks/src/hard/medianOfTwoSortedArrays/Solution.java
@@ -0,0 +1,46 @@
+package hard.medianOfTwoSortedArrays;
+
+class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(findMedianSortedArrays(
+                new int[]{1, 5, 7, 9, 10, 12},
+                new int[]{1, 2, 3, 6, 8, 15}));
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+        int left = 0, right = m;
+
+        while (left <= right) {
+            int partitionX = (left + right) / 2;
+            int partitionY = (m + n + 1) / 2 - partitionX;
+
+            int maxLeftX = (partitionX == 0) ? Integer.MIN_VALUE : nums1[partitionX - 1];
+            int minRightX = (partitionX == m) ? Integer.MAX_VALUE : nums1[partitionX];
+
+            int maxLeftY = (partitionY == 0) ? Integer.MIN_VALUE : nums2[partitionY - 1];
+            int minRightY = (partitionY == n) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(maxLeftX, maxLeftY)
+                            + Math.min(minRightX, minRightY)) / 2.0;
+                } else {
+                    return Math.max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
+            } else {
+                left = partitionX + 1;
+            }
+        }
+
+        throw new IllegalArgumentException("Arrays not sorted");
+    }
+}
