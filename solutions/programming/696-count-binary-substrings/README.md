82e8128f71de804e23f8dcd9b3af41319356d063
vovik541 <conanzhill@gmail.com>
2026-02-18 16:22:08 -0800

    Daily - 696. Count Binary Substrings - add solution

---
+++ solutions/programming/696-count-binary-substrings/README.md
@@ -0,0 +1,8 @@
+# 696. Count Binary Substrings
+
+- Link: https://leetcode.com/problems/count-binary-substrings/description
+- Difficulty: Easy
+- Tags: Staff, Two Pointers, String
+- Source: Daily 
+- Time: O(s.length)
+- Space: O(1)
---
+++ solutions/programming/696-count-binary-substrings/java/Solution.java
@@ -0,0 +1,20 @@
+package dev.vkh.solutions;
+
+public class Solution {
+  public int countBinarySubstrings(String s) {
+    int cur = 1, pre = 0, res = 0;
+    for (int i = 1; i < s.length(); i++) {
+      if (s.charAt(i) == s.charAt(i - 1)) cur++;
+      else {
+        res += Math.min(cur, pre);
+        pre = cur;
+        cur = 1;
+      }
+    }
+    return res + Math.min(cur, pre);
+  }
+
+  static void main() {
+    System.out.println(new Solution().countBinarySubstrings("00110011"));
+  }
+}
