74361f55cdafa618551a0de2fd3d40638a8fe9e0
vovik541 <conanzhill@gmail.com>
2026-03-03 17:19:48 -0800

    Daily - 1582. Special Positions in a Binary Matrix - add wrong solution 11/95

--- java-workspace/src/main/java/dev/vkh/solutions/Solution.java
+++ java-workspace/src/main/java/dev/vkh/solutions/Solution.java
@@ -1,5 +1,42 @@
 package dev.vkh.solutions;
 
 class Solution {
-  static void main() {}
+
+  public int numSpecial(int[][] mat) {
+    boolean notSpottedInRaw;
+    boolean notSpottedInCol;
+
+    int ans = 0;
+
+    nextRaw:
+    for (int i = 0; i < mat.length; i++) {
+      notSpottedInRaw = true;
+      notSpottedInCol = true;
+      for (int j = 0; j < mat[i].length; j++) {
+        if (mat[i][j] == 1) {
+          if (notSpottedInRaw) {
+            for (int h = 0; h < mat.length; h++) {
+              if (mat[h][j] == 1) {
+                if (notSpottedInCol) {
+                  notSpottedInCol = false;
+                } else {
+                  continue nextRaw;
+                }
+              }
+            }
+            notSpottedInRaw = false;
+          } else {
+            continue nextRaw;
+          }
+        }
+      }
+      ans += 1;
+    }
+
+    return ans;
+  }
+
+  static void main() {
+    System.out.println(new Solution().numSpecial(new int[][] {{1, 0, 0}, {0, 0, 1}, {1, 0, 0}}));
+  }
 }
---
+++ solutions/programming/1582-special-positions-in-a-binary-matrix/README.md
@@ -0,0 +1,8 @@
+# 1582. Special Positions in a Binary Matrix
+
+- Link: https://leetcode.com/problems/special-positions-in-a-binary-matrix/description
+- Difficulty: Easy
+- Tags: Mid Level, Array, Matrix
+- Source: Daily
+- Time: O(n)
+- Space: O(1)
