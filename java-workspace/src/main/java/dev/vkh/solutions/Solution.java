3a68b7acded49f43c920d18612ed5a207cbbb3bb
vovik541 <conanzhill@gmail.com>
2026-03-04 17:22:38 -0800

    main cleanup

--- java-workspace/src/main/java/dev/vkh/solutions/Solution.java
+++ java-workspace/src/main/java/dev/vkh/solutions/Solution.java
@@ -2,48 +2,8 @@ package dev.vkh.solutions;
 
 class Solution {
 
-  public int numSpecial(int[][] mat) {
-    int specialPositionsCount = 0;
-
-    nextRow:
-    for (int rowIndex = 0; rowIndex < mat.length; rowIndex++) {
-      boolean foundOneInRow = false;
-      int candidateColumnIndex = -1;
-
-      for (int colIndex = 0; colIndex < mat[rowIndex].length; colIndex++) {
-        if (mat[rowIndex][colIndex] == 1) {
-          if (foundOneInRow) {
-            continue nextRow;
-          }
-          foundOneInRow = true;
-          candidateColumnIndex = colIndex;
-        }
-      }
-
-      if (!foundOneInRow) {
-        continue;
-      }
-
-      for (int checkRowIndex = 0; checkRowIndex < mat.length; checkRowIndex++) {
-        if (checkRowIndex != rowIndex && mat[checkRowIndex][candidateColumnIndex] == 1) {
-          continue nextRow;
-        }
-      }
-
-      specialPositionsCount++;
-    }
-
-    return specialPositionsCount;
-  }
-
   static void main() {
     System.out.println(
-        new Solution()
-            .numSpecial(
-                new int[][] {
-                  {1, 0, 0},
-                  {0, 0, 1},
-                  {1, 0, 0}
-                })); // 1
+        new Solution());
   }
 }
