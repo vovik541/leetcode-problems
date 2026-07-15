c6bd87c88ae08d368ad17326811273cc44370ef4
vovik541 <conanzhill@gmail.com>
2026-07-14 20:12:40 -0700

    Daily - 3658. GCD of Odd and Even Sums - add solution

---
+++ solutions/programming/3658-gcd-of-odd-and-even-sums/README.md
@@ -0,0 +1,8 @@
+# 3658. GCD of Odd and Even Sums
+
+- Link: https://leetcode.com/problems/gcd-of-odd-and-even-sums/description
+- Difficulty: Easy
+- Tags: Mid Level, Math, Number Theory
+- Source: Daily
+- Time: O(n)
+- Space: O(n)
---
+++ solutions/programming/3658-gcd-of-odd-and-even-sums/java/Solution.java
@@ -0,0 +1,12 @@
+package dev.vkh.solutions;
+
+class Solution {
+  public static int gcdOfOddEvenSums(int n) {
+    return n;
+  }
+
+  static void main() {
+    System.out.println(gcdOfOddEvenSums(4)); //4
+    System.out.println(gcdOfOddEvenSums(5)); //5
+  }
+}
