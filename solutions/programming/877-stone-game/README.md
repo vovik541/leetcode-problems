8f9b6e9df0a4313280ebd3c1e71ed28207a784eb
vovik541 <conanzhill@gmail.com>
2026-08-01 18:37:26 -0700

    Daily - 877. Stone Game - add solution

---
+++ solutions/programming/877-stone-game/README.md
@@ -0,0 +1,8 @@
+# 877. Stone Game
+
+- Link: https://leetcode.com/problems/stone-game/description/
+- Difficulty: Medium
+- Tags: Staff, Array, Math, Dynamic Programming, Game Theory
+- Source: Daily
+- Time: O(1)
+- Space: O(1)
---
+++ solutions/programming/877-stone-game/java/Solution.java
@@ -0,0 +1,12 @@
+package dev.vkh.solutions;
+
+class Solution {
+  public static boolean stoneGame(int[] piles) {
+    return true;
+  }
+
+  static void main() {
+    System.out.println(stoneGame(new int[] {5,3,4,5})); // true
+    System.out.println(stoneGame(new int[] {3,7,2,3})); //  true
+  }
+}
