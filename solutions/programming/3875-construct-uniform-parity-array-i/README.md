8214e609a3e3a33540d32d79240161b326b0a395
vovik541 <conanzhill@gmail.com>
2026-09-01 17:46:34 -0700

    Daily - 3875. Construct Uniform Parity Array I - add solution

---
+++ solutions/programming/3875-construct-uniform-parity-array-i/README.md
@@ -0,0 +1,8 @@
+# 3875. Construct Uniform Parity Array I
+
+- Link: https://leetcode.com/problems/construct-uniform-parity-array-i/description
+- Difficulty: Easy
+- Tags: Mid Level, Array, Math
+- Source: Daily
+- Time: O(1)
+- Space: O(1)
---
+++ solutions/programming/3875-construct-uniform-parity-array-i/java/Solution.java
@@ -0,0 +1,12 @@
+package dev.vkh.solutions;
+
+class Solution {
+  public static boolean uniformArray(int[] nums1) {
+    return true;
+  }
+  static void main() {
+    System.out.println(uniformArray(new int[]{2,3})); // true
+    System.out.println(uniformArray(new int[]{4,6})); // true
+
+  }
+}
